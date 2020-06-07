a53e922449048b1f769591d0d1979c0ca287417e
Josh Zwiebel <joshzwiebel@gmail.com>
2020-06-07 19:28:03 -0400

    edit

--- server.py
+++ server.py
@@ -2,29 +2,29 @@ from flask import Flask, render_template, request
 
 from text_generation import generate_next_word
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('template.html', generated_text="your text will appear here")
 
 
-@app.route('/', methods=['POST'])
+@application.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
     texty = generate_next_word(str(text))
     return render_template('template.html', generated_text=texty)
 
 
-with app.app_context():
+with application.app_context():
     from text_generation import initialize
 
     initialize()
 
 
 def run():
-    app.run(debug=True, port=33507, use_reloader=False)
+    application.run(debug=True, port=33507, use_reloader=False)
 
 
 if __name__ == '__main__':
