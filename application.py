67e43641e8f24dcc062a38de5bb7e7e9fc79cf8a
Josh Zwiebel <joshzwiebel@gmail.com>
2020-06-09 20:43:40 -0400

    Update application.py

--- application.py
+++ application.py
@@ -3,29 +3,26 @@ from flask import Flask, render_template, request
 from text_generation import generate_next_word
 from text_generation import initialize
 
-application = Flask(__name__)
-application.secret_key = "Jl6EcFIbOe23EgdJ32MQ5WxmDz3bQ3L7KSDtVPij"
+app = Flask(__name__)
+app.secret_key = "Jl6EcFIbOe23EgdJ32MQ5WxmDz3bQ3L7KSDtVPij"
 
-@application.route('/')
+
+@app.route('/')
 def index():
     return render_template('template.html', generated_text="your text will appear here")
 
 
-@application.route('/', methods=['POST'])
+@app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
     texty = generate_next_word(str(text))
     return render_template('template.html', generated_text=texty)
 
 
-@application.before_first_request
+@app.before_first_request
 def initiate():
     initialize()
 
 
-
-
-
-
 if __name__ == '__main__':
-    application.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=False)
